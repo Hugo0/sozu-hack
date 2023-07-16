b9d0aba4df2b45098de6f5751b93f0742d4e1cd7
eerkaijun <kaijuneer@gmail.com>
2023-07-16 23:44:47 +0200

    setup

---
+++ test/Safe.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Safe.sol";
+
+contract SafeTest is Test {
+    Safe public safe;
+
+    function setUp() public {
+        safe = new Safe();
+    }
+}
